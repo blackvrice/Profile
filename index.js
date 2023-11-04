8a5f761804a8ab478ba7a8b2ef88a832e6feb6cf
oh seongsik <blackvrice@naver.com>
2023-11-04 14:50:30 +0900

    Html 생성

---
+++ index.js
@@ -0,0 +1,12 @@
+function MenuLayout(){
+    let div = document.createElement('div');
+    let ul = document.createElement('ul');
+    let menuList = ["Main","Game","Algorithm"];
+    menuList.forEach((value) => {
+        let li = document.createElement("li");
+        li.innerHTML = value;
+        ul.appendChild(li);
+    })
+    div.appendChild(ul);
+    document.body.appendChild(div);    
+}
