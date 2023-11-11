857bd0861c3b32c888cca960f7c57921bb871ee9
oh seongsik <blackvrice@naver.com>
2023-11-11 15:31:35 +0900

    오류 수정

--- js/Algorithm.js
+++ js/Algorithm.js
@@ -2,6 +2,11 @@ let Algorithm = {
     xhttp : new XMLHttpRequest(),
     QuestDiv : document.getElementById('Quest'),
     openQuest : function(directory,quest){
+        this.xhttp.onreadystatechange = function(){
+            if (this.readyState == 4 && this.status == 200) {
+                Algorithm.QuestDiv.innerHTML = this.responseText;
+            }
+        };
         this.xhttp.open('GET','./'+directory+'/'+quest+'.html');
         this.xhttp.send();
     },
@@ -9,8 +14,3 @@ let Algorithm = {
 
     }
 }
-Algorithm.xhttp.onreadystatechange = function(){
-    if (this.readyState == 4 && this.status == 200) {
-        this.QuestDiv.innerHTML = this.responseText;
-    }
-};
