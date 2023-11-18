9616205b11209a17fea5f8cc4e95df05356f5507
oh seongsik <blackvrice@naver.com>
2023-11-18 15:25:24 +0900

    Leve1

--- js/Algorithm.js
+++ js/Algorithm.js
@@ -1,16 +1,13 @@
 let Algorithm = {
     xhttp : new XMLHttpRequest(),
     QuestDiv : document.getElementById('Quest'),
-    openQuest : function(directory,quest){
+    openQuest : function(quest){
         this.xhttp.onreadystatechange = function(){
             if (this.readyState == 4 && this.status == 200) {
                 Algorithm.QuestDiv.innerHTML = this.responseText;
             }
         };
-        this.xhttp.open('GET','./'+directory+'/'+quest+'.html');
+        this.xhttp.open('GET','./Algorithm/'+quest+'.html');
         this.xhttp.send();
-    },
-    openSource : function(){
-
     }
 }
