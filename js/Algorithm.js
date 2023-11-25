c1329be6bd700480b1f1225babfb8c78ac21a6f6
oh seongsik <blackvrice@naver.com>
2023-11-25 14:04:03 +0900

    코드 테스트

--- js/Algorithm.js
+++ js/Algorithm.js
@@ -1,7 +1,14 @@
 let Algorithm = {
     xhttp : new XMLHttpRequest(),
+    quest : '',
+    cpp : '',
+    java : '',
+    cs : '',
+    python : '',
+    kotlin : '',
     openQuest : function(quest){
-        var div = document.getElementById('Quest');
+        this.quest = quest;
+        let div = document.getElementById('Quest');
         this.xhttp.onreadystatechange = function(){
             if (this.readyState == 4 && this.status == 200) {
                 div.innerHTML = this.responseText;
@@ -9,5 +16,23 @@ let Algorithm = {
         };
         this.xhttp.open('GET','./Algorithm/'+quest+'.html');
         this.xhttp.send();
+    },
+    openQuest : function(languge){
+        let script = document.getElementById('source');
+        let url = '';
+        switch (languge){
+            case "cpp":
+                url = this.cpp;
+                break;
+            case "java":
+                break;
+            case "cs":
+                break;
+            case "python":
+                break;
+            case "kotlin":
+                break;
+        }
+        
     }
 }
