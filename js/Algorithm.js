25a0bc24e662b2477c8058193b529523d3d5ce20
오성식 <blackvrice@naver.com>
2025-01-07 23:12:34 +0900

    포토폴리오 수정정

--- js/Algorithm.js
+++
@@ -1,8 +0,0 @@
-Category.page = 'Algorithm'
-
-function openSource(langague){
-    document.querySelectorAll('.language').forEach((value) =>{
-        value.style.display = "";
-    })
-    document.getElementById(langauge).style.display='block';
-}
--- js/Profile.js
+++
@@ -1,9 +0,0 @@
-window.addEventListener('load',(e)=>{
-    const birth = new Date(1997,1,23);
-    const today = new Date();
-    let age = today.getFullYear() - birth.getFullYear();
-    if((birth.getMonth() > today.getMonth())||(birth.getMonth() == today.getMonth() && birth.getDate() > today.getDate())){
-        age--;
-    }
-    document.getElementById('age').innerText = age;
-});
